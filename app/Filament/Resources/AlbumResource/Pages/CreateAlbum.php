66f0b32f0262594ef9abcad98a09d9295ff8c3de
Marco Introini <89445565+marco-introini@users.noreply.github.com>
2022-09-22 16:02:17 +0200

    albums administration

---
+++ app/Filament/Resources/AlbumResource/Pages/CreateAlbum.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Filament\Resources\AlbumResource\Pages;
+
+use App\Filament\Resources\AlbumResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateAlbum extends CreateRecord
+{
+    protected static string $resource = AlbumResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+}
